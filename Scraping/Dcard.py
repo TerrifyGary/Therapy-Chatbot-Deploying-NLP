d47edc89504319644ad7faccf209f4f530adda72
TerrifyGary <yoyoman1415926@gmail.com>
2022-07-18 15:50:38 +0800

    新增： 1. Dcard.py 完成 cloudscraper版的抓取 2. 可以像以前一樣存入可以像以前一樣存入db資料庫

--- Scraping/Dcard.py
+++ Scraping/Dcard.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import sqlite3
+import cloudscraper
 
 conn = sqlite3.connect('./Scraping/Data/articleList.db')
 
@@ -17,8 +18,6 @@ def get_web_info(topic,popular,length):
     result = requests.get(f"https://www.dcard.tw/service/api/v2/forums/{topic}/posts?popular={popular}&limit={str(length)}")
     data = json.loads(result.text)
 
-    data[0]['id']
-
     for x in range(length):
         articleID = (data[x]['id'])
         articleTitle = (data[x]['title'])
@@ -30,10 +29,70 @@ def get_web_info(topic,popular,length):
         c.execute('INSERT INTO articles VALUES (?,?,?,?,?,?)', temp_list)
         print(articleID, articleTitle)
 
+def new_get_web_info(topic,popular,length):
+    c = conn.cursor()
+    url = f"https://www.dcard.tw/service/api/v2/forums/{topic}/posts?popular={popular}&limit={str(length)}"
 
-res = get_web_info("mood","false",99)
+    #----- 抓取網頁的html -----#
+    isTarget = False
+    while isTarget==False:
+        scraper = cloudscraper.CloudScraper()  # CloudScraper inherits from requests.Session
+        scraper = cloudscraper.create_scraper()  # returns a CloudScraper instance
+        result = scraper.get(url).text
+        if result[0]!='<':
+            isTarget = True
+            break
+    
+    #----- 切串切割與整理 -----#
+    data = result.replace('{','')
+    data = data.replace('}','')
+    data = data.replace('[','')
+    data = data.replace(']','')
+    data = data.replace('"','')
+    output = data.split(',')
+
+    #----- 抓取要的重點 -----#
+    allID, allTitle, allExcerpt, allWithImages, allWithVideos = [], [], [], [], []
+    keyID, keyTitle, keyExcerpy, keyWithImages, keyWithVideos = 'id:', 'tit', 'exc', 'withIm', 'withVi'
+    for x in output:
+        if x[:3] == keyID and len(x)<=13:
+            temp = x
+            temp = temp.split(":")[-1]
+            allID.append(temp)
+        elif x[:3] == keyTitle:
+            temp = x
+            temp = temp.split(":")[-1]
+            allTitle.append(temp)
+        elif x[:3] == keyExcerpy:
+            temp = x
+            temp = temp.split(":")[-1]
+            allExcerpt.append(temp)
+        if x[:6] == keyWithImages:
+            temp = x
+            temp = temp.split(":")[-1]
+            allWithImages.append(temp)
+        elif x[:6] == keyWithVideos:
+            temp = x
+            temp = temp.split(":")[-1]
+            allWithVideos.append(temp)
+
+    #----- 可以存進去資料庫了 -----#
+    for x in range(length):
+        articleID = (allID[x])
+        articleTitle = (allTitle[x])
+        articleExcerpt = (allExcerpt[x])
+        articleWithImages = (allWithImages[x])
+        articleWithVideos = (allWithVideos[x])
+        articleCategories = topic
+        temp_list = [articleID,articleCategories,articleTitle,articleExcerpt,articleWithImages,articleWithVideos]
+        c.execute('INSERT INTO articles VALUES (?,?,?,?,?,?)', temp_list)
+        print(articleID, articleTitle)
+
+# res = get_web_info("mood","false",99)
+res = new_get_web_info("mood","true",99)
 
 conn.commit()
 conn.close()
 
 # createDataStruct()
+#----- 嗚嗚嗚嗚 為什麼沒事要加上那個害我全部都要重改 希望不要再被改了哭哭 -----#
--- Scraping/getEmotion.py
+++ Scraping/getEmotion.py
@@ -1,19 +1,9 @@
-from bs4 import BeautifulSoup
-# from fake_useragent import UserAgent
-import requests
-# from selenium import webdriver
-# from selenium.webdriver.common.keys import Keys
-import time
-import random
-import json
-import sqlite3
 import cloudscraper
 
 def main():
-    #------- 抓取目標文章資訊 -------#
-    
-    print(getEmotions('239432528'))
+    print(getEmotions('239465600'))
 
+#------- 字元轉換成數字 -------#
 def convertDigit(words):
     temp = ""
     for c in words:
@@ -25,7 +15,7 @@ def convertDigit(words):
     return temp
 
 def getEmotions(postID):
-
+    #------- 抓取目標文章資訊 -------#
     url = "https://www.dcard.tw/service/api/v2/posts/"+postID
     isTarget = False
 
@@ -43,56 +33,65 @@ def getEmotions(postID):
     data = data.replace('"','')
     output = data.split(',')
 
+    #------- 宣告尋找目標 -------#
     isAllEmotionError = True
+    heartReaction = "reactions:[id:286f599c-f86a-4932-82f0-f5a06f1eca03"
+    laughReaction = "id:e8e6bc5d-41b0-4129-b134-97507523d7ff"
+    shockReaction = "id:4b018f48-e184-445f-adf1-fc8e04ba09b9"
+    cryReaction = "id:514c2569-fd53-4d9d-a415-bf0f88e7329f"
+    madReaction = "id:aa0d425f-d530-4478-9a77-fe3aedc79eea"
+    admiredReaction = "id:011ead16-9b83-4729-9fde-c588920c6c2d"
     
-    if  "reactions:[id:286f599c-f86a-4932-82f0-f5a06f1eca03" in output:
+    #------- 條件式抓取目標 -------#
+    if  heartReaction in output:
         isAllEmotionError = False
-        labelHeart = output.index("reactions:[id:286f599c-f86a-4932-82f0-f5a06f1eca03")
+        labelHeart = output.index(heartReaction)
     else:
         print("labelHeart Error")
         labelHeart = 0
     
-    if "id:e8e6bc5d-41b0-4129-b134-97507523d7ff" in output:
+    if laughReaction in output:
         isAllEmotionError = False
-        labelLaugh = output.index("id:e8e6bc5d-41b0-4129-b134-97507523d7ff")
+        labelLaugh = output.index(laughReaction)
     else:
         print("labelLaugh Error")
         labelLaugh = 0
 
-    if "id:4b018f48-e184-445f-adf1-fc8e04ba09b9" in output:
+    if  shockReaction in output:
         isAllEmotionError = False
-        labelShock = output.index("id:4b018f48-e184-445f-adf1-fc8e04ba09b9")
+        labelShock = output.index(shockReaction)
     else:
         print("labelShock Error")
         labelShock = 0
 
-    if "id:514c2569-fd53-4d9d-a415-bf0f88e7329f" in output:
+    if cryReaction in output:
         isAllEmotionError = False
-        labelCry = output.index("id:514c2569-fd53-4d9d-a415-bf0f88e7329f")
+        labelCry = output.index(cryReaction)
     else:
         print("labelCry Error")
         labelCry = 0
 
-    if "id:aa0d425f-d530-4478-9a77-fe3aedc79eea" in output:
+    if madReaction in output:
         isAllEmotionError = False
-        labelMad = output.index("id:aa0d425f-d530-4478-9a77-fe3aedc79eea")
+        labelMad = output.index(madReaction)
     else:
         print("labelMad Error")
         labelMad = 0
 
-    if "id:011ead16-9b83-4729-9fde-c588920c6c2d" in output:
+    if admiredReaction in output:
         isAllEmotionError = False
-        labelAdmired = output.index("id:011ead16-9b83-4729-9fde-c588920c6c2d")
+        labelAdmired = output.index(admiredReaction)
     else:
         print("labelAdmired Error")
         labelAdmired = 0
 
+    #------- 判斷情緒是否都有抓到 -------#
+    #------- 並將目標轉換成數字 -------#
     if  isAllEmotionError == True:
         # print("Emotional Error")
         return "Emotional Error."
     else:
         mood = [output[labelHeart+1], output[labelLaugh+1], output[labelShock+1], output[labelCry+1], output[labelMad+1], output[labelAdmired+1]]
-        
         numbers = []
         for x in mood:
             numbers.append(convertDigit(x))
