ed473bd1e949a5d0757f42fa1e3e793926f6f5a4
TerrifyGary <yoyoman1415926@gmail.com>
2022-11-23 11:46:39 +0800

    Titles Count = 38671

--- Scraping/getTitleReactions.py
+++ Scraping/getTitleReactions.py
@@ -210,14 +210,14 @@ def get_web_info_cloudscraper(topic,popular):
 def main():
 
 
-    get_web_info_selenium("mood","true")
-    get_web_info_selenium("mood","false")
-    get_web_info_selenium("relationship","true")
+    # get_web_info_selenium("mood","true")
+    # get_web_info_selenium("mood","false")
+    # get_web_info_selenium("relationship","true")
 
 
-    # get_web_info_selenium("relationship","false")
-    # get_web_info_selenium("talk","true")
-    # get_web_info_selenium("talk","false")
+    get_web_info_selenium("relationship","false")
+    get_web_info_selenium("talk","true")
+    get_web_info_selenium("talk","false")
 
 
 
